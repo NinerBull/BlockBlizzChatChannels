aa71e2d76e4ea2c65cb1178d39b05d8be3988abc
NinerBull <72740816+NinerBull@users.noreply.github.com>
2024-07-30 01:12:43 +0200

    First Commit

---
+++ BlockBlizzChatChannels/BlockBlizzChatChannels.lua
@@ -0,0 +1,359 @@
+
+local BlockBlizzChatChannels_Frame = CreateFrame("Frame")
+
+BlockBlizzChatChannels_Frame:RegisterEvent("CHANNEL_UI_UPDATE")
+BlockBlizzChatChannels_Frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+BlockBlizzChatChannels_Frame:RegisterEvent("SETTINGS_LOADED")
+BlockBlizzChatChannels_Frame:RegisterEvent("ADDON_LOADED")
+BlockBlizzChatChannels_Frame:RegisterEvent("CHANNEL_UI_UPDATE")
+
+
+local BlockBlizzChatChannels_TextName = FRIENDLY_STATUS_COLOR:WrapTextInColorCode("<Block Blizzard Chat Channels>")
+local BlockBlizzChatChannels_TextSlash = FRIENDLY_STATUS_COLOR:WrapTextInColorCode("/blockchatchannels")
+
+
+
+
+
+local function OnSettingChanged(_, setting, value)
+	local variable = setting:GetVariable()
+	BlockBlizzChatChannelsData[variable] = value
+	BlockBlizzChatChannels_Frame.CheckForChatBlock()
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+BlockBlizzChatChannels_Frame:SetScript("OnEvent", function(self, event, arg1, arg2)
+
+
+
+			if (event == "ADDON_LOADED" and arg1 == "BlockBlizzChatChannels") then
+				
+				if (type(BlockBlizzChatChannelsData) ~= "table") then
+					BlockBlizzChatChannelsData = {}
+					
+					print(BlockBlizzChatChannels_TextName .. " Addon Loaded for the first time! Type " .. BlockBlizzChatChannels_TextSlash .. " to configure.")
+					
+				end
+
+
+
+				local category, layout = Settings.RegisterVerticalLayoutCategory("Block Blizzard Chat Channels Account Wide")
+				
+				BlockBlizzChatChannels_OptionsPanelFrameCategoryID = category:GetID()
+
+
+				do
+					layout:AddInitializer(CreateSettingsListSectionHeaderInitializer("Select Channels to Block Below", "All characters on your account will be prevented from joining the channels you check below.\n\nYou will need to manually rejoin these channels if you change your mind later on.\n\nChanges take effect the next time you change zone, or relog or type the /reload command."));
+				end
+
+				do
+					local variable = "BlockGeneral"
+					local name = "Block General Chat"
+					local tooltip = "Checking this box will prevent all of your characters from joining the General Chat Channel.\nThis is the zone-wide public chat channel, and even works in instances."
+					local defaultValue = false
+
+					local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+					-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+					
+					Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+					if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+						Settings.CreateCheckbox(category, setting, tooltip)
+					else
+						Settings.CreateCheckBox(category, setting, tooltip)
+					end
+				end
+
+				do
+					local variable = "BlockTrade"
+					local name = "Block Trade Chat"
+					local tooltip = "Checking this box will prevent all of your characters from joining the Trade Chat Channel.\n\nThis channel is accessible in all major cities, and discussions can often fill the chat box very quickly during peak hours on populated realms."
+					local defaultValue = false
+
+					local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+					-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+					
+					Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+					if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+						Settings.CreateCheckbox(category, setting, tooltip)
+					else
+						Settings.CreateCheckBox(category, setting, tooltip)
+					end
+					
+					
+				end
+
+				
+				if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+					do
+						local variable = "BlockServices"
+						local name = "Block Services Chat"
+						local tooltip = "Checking this box will prevent all of your characters from joining the Services Chat Channel.\n\nThis channel was introduced in 9.2.7 and is accessible in major cities.\n\nIt is mostly used by players selling Raid/M+/PVP Boosting/Carrying services and the like, and it can get extremely spammy on some populated realms."
+						local defaultValue = false
+
+						local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+					-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+						
+						Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+						if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+							Settings.CreateCheckbox(category, setting, tooltip)
+						else
+							Settings.CreateCheckBox(category, setting, tooltip)
+						end
+						
+					end
+				end
+				
+				
+				do
+					local variable = "BlockLocalDefense"
+					local name = "Block LocalDefense Chat"
+					local tooltip = "Checking this box will prevent all of your characters from joining the LocalDefense Chat Channel.\n\nThis channel is zone-wide, and will show 'X is under attack!' notices when a friendly NPC is killed by the opposite faction, which could get spammy in War Mode/PVP Realms."
+					local defaultValue = false
+
+					local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+					-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+					
+					Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+					if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+						Settings.CreateCheckbox(category, setting, tooltip)
+					else
+						Settings.CreateCheckBox(category, setting, tooltip)
+					end
+					
+				end
+				
+				
+				if (WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE) then
+					do
+						local variable = "BlockWorldDefense"
+						local name = "Block WorldDefense Chat"
+						local tooltip = "Checking this box will prevent all of your characters from joining the WorldDefense Chat Channel.\n\nSimiar to LocalDefense, except it will show attack notices for the entire world, rather than just the zone you are in."
+						local defaultValue = false
+
+						local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+						-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+						
+						Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+						if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+							Settings.CreateCheckbox(category, setting, tooltip)
+						else
+							Settings.CreateCheckBox(category, setting, tooltip)
+						end
+						
+					end
+				end
+				
+				
+				do
+					local variable = "BlockLookingForGroup"
+					local name = "Block LookingForGroup Chat"
+					local tooltip = "Checking this box will prevent all of your characters from joining the LookingForGroup Chat Channel.\n\nThis channel is realm-wide. It's meant to help find groups for content, and can see heavy use on Cataclysm and Era Realms.\n\nLeaving this channel won't prevent you from using the Dungeon/Raid/Group Finders in Retail/Cataclysm, but addons such as 'LFG Group Bulletin Board' rely on being in the channel to work."
+					local defaultValue = false
+
+					local setting = Settings.RegisterAddOnSetting(category, name, variable, type(defaultValue), (BlockBlizzChatChannelsData[variable] or defaultValue))
+					-- local setting = Settings.RegisterAddOnSetting(category, variable, variable, BlockBlizzChatChannelsData, type(defaultValue), name, (BlockBlizzChatChannelsData[variable] or defaultValue))
+					
+					Settings.SetOnValueChangedCallback(variable, OnSettingChanged)
+					if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+						Settings.CreateCheckbox(category, setting, tooltip)
+					else
+						Settings.CreateCheckBox(category, setting, tooltip)
+					end
+				end
+				
+				do
+					layout:AddInitializer(CreateSettingsListSectionHeaderInitializer("Rejoin Blizzard Chat Channels"));
+				end
+				
+				do
+					local function OnButtonClick()
+						JoinPermanentChannel("General")
+						JoinPermanentChannel("LocalDefense")
+						JoinPermanentChannel("Trade")
+						JoinPermanentChannel("LookingForGroup")
+						if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+							JoinPermanentChannel("Services")
+						else
+							JoinPermanentChannel("WorldDefense")
+						end
+						BlockBlizzChatChannels_Frame.CheckForChatBlock()
+						print(BlockBlizzChatChannels_TextName .. " Rejoined all channels, where possible.")
+					end
+
+					local addSearchTags = true;
+					local initializer = CreateSettingsButtonInitializer("Rejoin All Channels", "Rejoin", OnButtonClick, "Click to attempt to rejoin all the default chat channels on your current character.\n\nYou won't join any channels you have blocked above.", addSearchTags);
+					layout:AddInitializer(initializer);
+				end
+
+				Settings.RegisterAddOnCategory(category)
+				
+				
+				SLASH_BLOCKBLIZZCHATCHANNELS1 = "/blockchatchannels"
+				function SlashCmdList.BLOCKBLIZZCHATCHANNELS(msg)
+					Settings.OpenToCategory(BlockBlizzChatChannels_OptionsPanelFrameCategoryID)
+				end
+				
+				
+			end
+			
+			
+			-- Remove player from channels if they're blocked
+			if (event == "CHANNEL_UI_UPDATE") then
+	
+				BlockBlizzChatChannels_Frame.CheckForChatBlock()
+			
+			end
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+end)
+
+
+
+
+function BlockBlizzChatChannels_Frame:CheckForChatBlock()
+
+	if (BlockBlizzChatChannelsData["BlockGeneral"] == true) then
+		if (GetChannelName((GetChannelName("General"))) > 0) then
+			LeaveChannelByName("General")
+		end
+	end
+	
+	if (BlockBlizzChatChannelsData["BlockLocalDefense"] == true) then
+		if (GetChannelName((GetChannelName("LocalDefense"))) > 0) then
+			LeaveChannelByName("LocalDefense")
+		end
+	end
+	
+	if (BlockBlizzChatChannelsData["BlockTrade"] == true) then
+		if (GetChannelName((GetChannelName("Trade"))) > 0) then
+			LeaveChannelByName("Trade")
+		end
+	end
+	
+	if (BlockBlizzChatChannelsData["BlockLookingForGroup"] == true) then
+		if (GetChannelName((GetChannelName("LookingForGroup"))) > 0) then
+			LeaveChannelByName("LookingForGroup")
+		end
+	end
+	
+	if (WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE) then
+		if (BlockBlizzChatChannelsData["BlockWorldDefense"] == true) then
+			if (GetChannelName((GetChannelName("WorldDefense"))) > 0) then
+				LeaveChannelByName("WorldDefense")
+			end
+		end
+	end
+	
+	if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+		if (BlockBlizzChatChannelsData["BlockServices"] == true) then
+			if (GetChannelName((GetChannelName("Services"))) > 0) then
+				LeaveChannelByName("Services")
+			end
+		end
+	end
+
+
+end
+
+
+
+
+function BlockBlizzChatChannels_Frame:IsChatBlockActive(thisChannel)
+	
+		if (BlockBlizzChatChannelsData[thisChannel] == true) then
+			return("|TInterface\\RaidFrame\\ReadyCheck-Ready:0|t")
+		else
+			return("|TInterface\\RaidFrame\\ReadyCheck-NotReady:0|t")
+		end	
+	
+	end
+
+--Addon Compartment
+	local BlockBlizzChatChannels_Tooltip
+
+	function BlockBlizzChatChannels_CompartmentClick(addonName, buttonName)
+		Settings.OpenToCategory(BlockBlizzChatChannels_OptionsPanelFrameCategoryID)
+	end
+
+	function BlockBlizzChatChannels_CompartmentHover(addonName, buttonName)
+	
+			if (not BlockBlizzChatChannels_Tooltip) then
+				BlockBlizzChatChannels_Tooltip = CreateFrame("GameTooltip", "BlockBlizzChatChannels_Tooltip_Compartment", UIParent, "GameTooltipTemplate")
+			end
+			
+			local classColorString = C_ClassColor.GetClassColor(UnitClass("player")) or NORMAL_FONT_COLOR
+			
+			BlockBlizzChatChannels_Tooltip:SetOwner(buttonName, "ANCHOR_LEFT");
+			BlockBlizzChatChannels_Tooltip:SetText("Block Blizzard Chat Channels")
+			
+			BlockBlizzChatChannels_Tooltip:AddLine(" ")
+			BlockBlizzChatChannels_Tooltip:AddLine("Current Settings:",  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			BlockBlizzChatChannels_Tooltip:AddLine(" ")
+			
+			
+			BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking General Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockGeneral"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking Trade Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockTrade"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			if (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE) then
+			
+				BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking Services Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockServices"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			end
+			
+			BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking LocalDefense Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockLocalDefense"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			if (WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE) then
+			
+				BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking WorldDefense Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockWorldDefense"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			
+			end
+			
+			BlockBlizzChatChannels_Tooltip:AddDoubleLine("Blocking LookingForGroup Chat" .. ":", BlockBlizzChatChannels_Frame:IsChatBlockActive("BlockLookingForGroup"), nil, nil, nil,  WHITE_FONT_COLOR.r, WHITE_FONT_COLOR.g, WHITE_FONT_COLOR.b)
+			
+			
+			
+			BlockBlizzChatChannels_Tooltip:AddLine(" ")
+			BlockBlizzChatChannels_Tooltip:AddLine("Click to change settings.",  GREEN_FONT_COLOR.r, GREEN_FONT_COLOR.g, GREEN_FONT_COLOR.b)
+			
+			BlockBlizzChatChannels_Tooltip:Show()
+		
+	end
+
+	function BlockBlizzChatChannels_CompartmentLeave(buttonName)
+			BlockBlizzChatChannels_Tooltip:Hide()
+	end
+
+
+
+
+
+
+
+
+
+			
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# BlockBlizzChatChannels
-WoW Addon - Block various default Chat Channels such as Trade or Services, account wide.
+# Block Blizzard Chat Channels
+
+An addon for World of Warcraft - The War Within, Cataclysm Classic and Classic Era!
+
+## About
+This addon allows you to block several Blizzard-run chat channels in game across your entire account.
+
+It allows you prevent all of your characters from joining any (or all) of the following Channels:
+
+* General Chat
+* Trade Chat
+* Services Chat (Retail Only)
+* LocalDefense Chat
+* WorldDefense Chat (Classic/Era Only)
+* LookingForGroup Chat
+
+
+## Compatibility
+* **The War Within** - 11.0.0
+* **Cataclysm Classic** - 4.4.0
+* **Classic Era** - 1.15.3
+
+
+## Configuration
+By default, no chat channels are blocked, and you'll need to configure the addon to block only the channels you do not want to see.
+
+You can configure the addon by typing `/blockchatchannels` into chat, or from the 'Block Blizzard Chat Channels' section of the Game Options.
+
+Ticking a box will take effect immediately, and will affect all of your characters account wide.
+
+You can click the 'Rejoin' button to make your current character join all of the channels that you have not blocked.
